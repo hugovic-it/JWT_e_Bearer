77372359e5901c61272e6999de0a994e09fee419
Hugo Victor <hugovic.it@gmail.com>
2024-06-04 08:45:23 -0300

    03 - Criand oa classe de usuario

---
+++ JwtAspNet/Services/TokenService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtAspNet.Services;
+public class TokenService
+{
+    public string Create(){
+        return "";
+    }
+}
