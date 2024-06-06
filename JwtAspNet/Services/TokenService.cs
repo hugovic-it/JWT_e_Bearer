6903c0eed33a2d0c70f753bbd803343c795bf05e
Hugo Victor <hugovic.it@gmail.com>
2024-06-06 11:22:35 -0300

    06 - Gerando o Token

--- JwtAspNet/Services/TokenService.cs
+++ JwtAspNet/Services/TokenService.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JwtAspNet.Services;
 public class TokenService
@@ -10,7 +12,18 @@ public class TokenService
     public string Create(){
         var handler = new JwtSecurityTokenHandler();
 
-        var token = handler.CreateToken();
+        var key = Encoding.ASCII.GetBytes(Configuration.PrivateKey);
+
+        var credentials = new SigningCredentials(
+            new SymmetricSecurityKey(key),
+            algorithm:SecurityAlgorithms.HmacSha256 );
+        
+        var tokenDescriptor = new SecurityTokenDescriptor{
+            SigningCredentials = credentials,
+            Expires = DateTime.UtcNow.AddHours(2)
+        };
+
+        var token = handler.CreateToken(tokenDescriptor);
         return handler.WriteToken(token);
     }
 }
